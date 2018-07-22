54030070455ffffb2d724158ed16e76862c8eb35
Igor <igor.myth@gmail.com>
2018-07-22 20:52:08 +0300

    Changed layered bot

--- src/layered-bot.lisp
+++ src/layered-bot.lisp
@@ -8,7 +8,8 @@
           :src/model)
   (:import-from :alexandria)
   (:shadowing-import-from :src/commands
-                          #:fill))
+                          #:fill)
+  (:export #:a-star-path-to-dest))
 
 (in-package :src/layered-bot)
 
@@ -47,7 +48,7 @@
                      :sld1 (pos-diff (first coord-list) orig-coord)
                      :sld2 (pos-diff (second coord-list) (first coord-list)))))
 
-(defun shortest-path (start-coord targets state)
+(defun shortest-path (start-coord targets state &key y-limits)
   (let ((wave (make-hash-table :test #'equalp)))
     (labels ((%iter (coords)
                ;; (format t "Front: ~A~%" coords)
@@ -73,7 +74,12 @@
                            coord
                            (lambda (coord1)
                              (and (inside-field? coord1 (state-r state))
-                                  (not (voxel-full? state coord1))))
+                                  (not (voxel-full? state coord1))
+                                  (or (null y-limits)
+                                      (let ((y (aref coord1 1))
+                                            (start-y (aref start-coord 1)))
+                                        (and (>= y (min start-y (car y-limits)))
+                                             (<= y (max start-y (cdr y-limits))))))))
                            (lambda (coord-list)
                              (let ((coord1 (car (last coord-list))))
                                (when (null (gethash coord1 wave))
@@ -87,6 +93,100 @@
       (setf (gethash start-coord wave)
             (list :start))
       (%iter (list start-coord)))))
+
+(defun a-star-path (start-coord targets targets-for-heuristics state)
+  (let ((wave (make-hash-table :test #'equalp))
+        (point-queue (make-instance 'cl-heap:priority-queue)))
+    (labels ((%prioroty (coord)
+               ;; (length (gethash coord wave))
+               (+ (length (gethash coord wave))
+                  (loop :for target :in targets-for-heuristics
+                     :minimizing (truncate (diff-lens target coord)
+                                           15)))
+               )
+             (%loop ()
+               (loop
+                  :do
+                  (let ((coord (cl-heap:dequeue point-queue)))
+                    (if coord
+                        (if (gethash coord targets)
+                            (return (values coord
+                                            (cdr (reverse (gethash coord wave)))))
+                            (progn
+                              (try-moves
+                               coord
+                               (lambda (coord1)
+                                 (and (inside-field? coord1 (state-r state))
+                                      (not (voxel-full? state coord1))))
+                               (lambda (coord-list)
+                                 (let ((coord1 (car (last coord-list))))
+                                   (when (null (gethash coord1 wave))
+                                     (setf (gethash coord1 wave)
+                                           (cons (coords-to-command coord coord-list)
+                                                 (gethash coord wave)))
+                                     (cl-heap:enqueue point-queue coord1 (%prioroty coord))))))))
+                        (return))))))
+      (setf (gethash start-coord wave)
+            (list :start))
+      (cl-heap:enqueue point-queue start-coord 0)
+      (%loop))))
+
+(defun a-star-path-to-dest (start-coord target-coord state)
+  (let ((wave (make-hash-table :test #'equalp))
+        (point-queue (make-instance 'cl-heap:priority-queue)))
+    (labels ((%prioroty (coord)
+               (+ (length (gethash coord wave))
+                  (truncate (diff-lens target-coord coord)
+                            15)))
+             (%loop ()
+               (loop
+                  :do
+                  (let ((coord (cl-heap:dequeue point-queue)))
+                    (if coord
+                        (if (pos-eq coord target-coord)
+                            (return (values coord
+                                            (cdr (reverse (gethash coord wave)))))
+                            (progn
+                              (try-moves
+                               coord
+                               (lambda (coord1)
+                                 (and (inside-field? coord1 (state-r state))
+                                      (not (voxel-full? state coord1))))
+                               (lambda (coord-list)
+                                 (let ((coord1 (car (last coord-list))))
+                                   (when (null (gethash coord1 wave))
+                                     (setf (gethash coord1 wave)
+                                           (cons (coords-to-command coord coord-list)
+                                                 (gethash coord wave)))
+                                     (cl-heap:enqueue point-queue coord1 (%prioroty coord))))))))
+                        (return))))))
+      (setf (gethash start-coord wave)
+            (list :start))
+      (cl-heap:enqueue point-queue start-coord 0)
+      (%loop))))
+
+(defun some-elems-from-set (hashset)
+  (loop
+     :for i :below 4
+     :for k :being :the :hash-keys :in hashset
+     :collect k))
+
+(defun y-limits (hashset)
+  (loop
+     :for k :being :the :hash-keys :in hashset
+     :minimizing (aref k 1) :into min-y
+     :maximizing (aref k 1) :into max-y
+     :finally (return (cons min-y max-y))))
+
+(defun shortest-path-a* (start-coord targets state)
+  ;; (shortest-path start-coord targets state)
+  (let ((targets-for-heuristics
+         (loop
+            :for i :below 4
+            :for k :being :the :hash-keys :in targets
+            :collect k)))
+    (a-star-path start-coord targets targets-for-heuristics state))
+  )
 
 (defun one-target-fill (coord fill-set target-set state)
   (mapc-near
@@ -162,12 +262,16 @@
                                  :do (setf (gethash coord tab) t))
                               tab)
            :for target-set := (fill-set-to-target-set fill-set state)
+           :for target-limits := (y-limits target-set)
            :do (loop
                   :do (progn
                         (format t "Step: ~A~%" (reduce #'+ (mapcar #'length res-trace)))
+                        (format t "Level: ~A/~A~%" level (hash-table-count level-fill-set))
                         (format t "Bot: ~A~%" bot-coord)
-                        (format t "Fill set: ~A~%" (alexandria:hash-table-keys fill-set))
-                        (format t "Target set: ~A~%" (alexandria:hash-table-keys target-set))
+                        (format t "Fill set size: ~A~%" (hash-table-count fill-set))
+                        (format t "Target set size: ~A~%" (hash-table-count target-set))
+                        ;; (format t "Fill set: ~A~%" (alexandria:hash-table-keys fill-set))
+                        ;; (format t "Target set: ~A~%" (alexandria:hash-table-keys target-set))
                         (let ((coord (find-to-fill bot-coord fill-set state)))
                           (format t "Can fill coord: ~A~%" coord)
                           (if coord
@@ -185,7 +289,11 @@
                                         (format t "Target point was not found~%")
                                         (return-from outer)))
                                   (multiple-value-bind (goto-coord moves)
-                                      (shortest-path bot-coord target-set state)
+                                      (multiple-value-bind (goto-coord1 moves1)
+                                          (shortest-path bot-coord target-set state :y-limits target-limits)
+                                        (if goto-coord1
+                                            (values goto-coord1 moves1)
+                                            (shortest-path bot-coord target-set state)))
                                     (format t "Then goto ~A~%" goto-coord)
                                     (if goto-coord
                                         (progn
